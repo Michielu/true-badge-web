5aa8bbd3c914d68037c6df5805c25da51e48b447
Michielu Menning <mich.menning@gmail.com>
2020-02-21 20:35:32 -0600

    Install dotenv to store env variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import db from './src/config/db';
 const app = express();
+dotenv.config();
+const db = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@ds163517.mlab.com:63517/true-badge";
 const port = 3000;
 
 app.use(bodyParser.urlencoded({
@@ -18,14 +20,10 @@ MongoClient.connect(db, {
     if (err) return console.log(err)
 
     // Make sure you add the database name and not the collection name
-    let datab = database.db("true-badge")
+    let datab = database.db("true-badge");
 
     // require('./routes')(app, datab);
     app.listen(port, () => {
         console.log('We are live on ' + port);
     });
 })
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
