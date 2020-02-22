272326b48863a97d9653425ae5a10e61fed6699d
Michielu Menning <mich.menning@gmail.com>
2020-02-22 11:39:30 -0600

    Initial jest setup and clean babel packages

---
+++ test/dotenv.test.js
@@ -0,0 +1,5 @@
+describe('Test dotenv processes', () => {
+    it('should test that true === true', () => {
+        expect(process.env.TEST).toBe("TESTING")
+    })
+})
---
+++ test/setup.js
@@ -0,0 +1,4 @@
+// TODO figure out how to use ES6 import from CommonJS's require
+// Probably has to do with transform in package.json/jest
+const dotenv = require('dotenv');
+dotenv.config();
