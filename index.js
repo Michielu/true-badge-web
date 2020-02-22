735a67951e266dfea2b4e0f4628d325a4e44b53b
Michielu Menning <mich.menning@gmail.com>
2020-02-21 20:23:12 -0600

    Instal babel to use ES6

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import db from './src/config/db';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+MongoClient.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, database) => {
+    if (err) return console.log(err)
+
+    // Make sure you add the database name and not the collection name
+    let datab = database.db("true-badge")
+
+    // require('./routes')(app, datab);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
