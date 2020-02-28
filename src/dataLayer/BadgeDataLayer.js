dee74b63b36adbe6ccc3b089c36552929229e559
Michielu Menning <mich.menning@gmail.com>
2020-02-27 20:58:48 -0600

    Apply async/await to Badge put

--- src/dataLayer/BadgeDataLayer.js
+++ src/dataLayer/BadgeDataLayer.js
@@ -1,18 +1,32 @@
 const collectionName = 'badge';
 const BadgeDataLayer = {
-    put: (db, badgeData) => {
+    put: async (db, badgeData) => {
         console.log("Badge Data is :", badgeData);
-        db.collection(collectionName).insertOne(badgeData, (err, result) => {
-            if (err) {
-                return ({
-                    'error': 'An error has occurred'
-                });
-            } else {
-                console.log("Result: ", result.ops[0])
-                return (result.ops[0]);
-            }
-        });
+        const res = await db.collection(collectionName).insertOne(badgeData);
+        // console.log("÷")
+        // return { "err": res.err, "result": res.result };
+        return { "err": res.err, "result": res.result };
+
+        //  (err, result) => {
+        //     if (err) {
+        //         return ({
+        //             'error': 'An error has occurred'
+        //         });
+        //     } else {
+        //         console.log("Result: ", result.ops[0])
+        //         return (result.ops[0]);
+        //     }
+        // });
     }
 }
 
 export default BadgeDataLayer;
+
+
+// try {
+//     // const res = await db.collection("collectionName").updateOne({ 
+//     //     "someKey": someValue
+//     // }, { $set: someObj }, { upsert: true });
+
+//     console.log(`res => ${JSON.stringify(res)}`);
+// }
