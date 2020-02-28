091f872575c1417f88d5c55ad2b705170619f359
Michielu Menning <mich.menning@gmail.com>
2020-02-27 21:00:03 -0600

    Small clean

--- src/dataLayer/BadgeDataLayer.js
+++ src/dataLayer/BadgeDataLayer.js
@@ -3,7 +3,6 @@ const BadgeDataLayer = {
     put: async (db, badgeData) => {
         console.log("Badge Data is :", badgeData);
         const res = await db.collection(collectionName).insertOne(badgeData);
-        // console.log("÷")
         // return { "err": res.err, "result": res.result };
         return { "err": res.err, "result": res.result };
 
@@ -21,12 +20,3 @@ const BadgeDataLayer = {
 }
 
 export default BadgeDataLayer;
-
-
-// try {
-//     // const res = await db.collection("collectionName").updateOne({ 
-//     //     "someKey": someValue
-//     // }, { $set: someObj }, { upsert: true });
-
-//     console.log(`res => ${JSON.stringify(res)}`);
-// }
