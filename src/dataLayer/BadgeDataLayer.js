b1d937eee87758e458f8ec7f29c01fe6b45d9014
Michielu Menning <mich.menning@gmail.com>
2020-02-28 21:33:50 -0600

    Return desired result from Badge put

--- src/dataLayer/BadgeDataLayer.js
+++ src/dataLayer/BadgeDataLayer.js
@@ -1,21 +1,8 @@
 const collectionName = 'badge';
 const BadgeDataLayer = {
     put: async (db, badgeData) => {
-        console.log("Badge Data is :", badgeData);
         const res = await db.collection(collectionName).insertOne(badgeData);
-        // return { "err": res.err, "result": res.result };
-        return { "err": res.err, "result": res.result };
-
-        //  (err, result) => {
-        //     if (err) {
-        //         return ({
-        //             'error': 'An error has occurred'
-        //         });
-        //     } else {
-        //         console.log("Result: ", result.ops[0])
-        //         return (result.ops[0]);
-        //     }
-        // });
+        return { "err": res.err, "result": res.ops[0] };
     }
 }
 
