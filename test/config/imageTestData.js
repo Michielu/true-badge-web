8bd66d7fe20791cd057c44fdde1ab6075bcf8651
Michielu Menning <mich.menning@gmail.com>
2020-03-11 21:15:15 -0500

    Initial imageTestData

---
+++ test/config/imageTestData.js
@@ -0,0 +1,23 @@
+const dataLayerPostData = {
+    description: null,
+    contentType: 'image/jpeg',
+    size: 90607,
+    img: ["Binary"]
+}
+
+const dataLayerGetReturnData = {
+    err: undefined,
+    result:
+        [{
+            _id: "5e6577536dec4f658d93f5c1",
+            description: null,
+            contentType: 'image/jpeg',
+            size: 90607,
+            img: ["Binary"]
+        }]
+}
+
+export default {
+    dataLayerPostData,
+    dataLayerGetReturnData
+}
