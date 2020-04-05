6d8cf36300a6b82b34b023512ac951c42588c8b4
Michielu Menning <mich.menning@gmail.com>
2020-04-04 19:31:01 -0500

    Create initial tests for audio's datalayer

---
+++ test/config/audioTestData.js
@@ -0,0 +1,23 @@
+const dataLayerPostData = {
+    description: null,
+    contentType: 'audio/webm',
+    size: 90607,
+    audio: ["Binary"]
+}
+
+const dataLayerGetReturnData = {
+    err: undefined,
+    result:
+        [{
+            _id: "5e6577536dec4f658d93f5c1",
+            description: null,
+            contentType: 'audio/webm',
+            size: 90607,
+            audio: ["Binary"]
+        }]
+}
+
+export default {
+    dataLayerPostData,
+    dataLayerGetReturnData
+}
---
+++ test/dataLayer/audioDataLayer.test.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+
+import AudioDataLayer from "../../src/dataLayer/AudioDataLayer.js";
+import testData from '../config/audioTestData';
+
+
+describe('Test Audio Routes', () => {
+    let connection;
+    let db;
+    let audioID;
+
+    beforeAll(async () => {
+        //process.env.MONGO_URL is cached locally in node_modules/.cache
+        connection = await MongoClient.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        db = await connection.db(process.env.TEST_DB_NAME);
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    it('test audio post: return data', async () => {
+        const res = await AudioDataLayer.put(db, testData.dataLayerPostData);
+        audioID = res.result;
+        //Cannot predict the audioID 
+        expect(res.err).toBe(undefined);
+    });
+
+    it('test badge post: stores badge', async () => {
+        const audioCollection = db.collection('audio');
+        const res2 = await audioCollection.findOne({ _id: new ObjectID(audioID) });
+        expect(res2).toEqual(testData.dataLayerPostData);
+    });
+});
