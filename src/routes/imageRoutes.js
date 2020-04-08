d740408f3755eb9935e39a2f1c814a2b7f714d66
Michielu Menning <mich.menning@gmail.com>
2020-04-07 20:18:33 -0500

    Image Route error handling

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -10,20 +10,29 @@ import ImageService from '../services/imageService';
 const BadgeImage = {
     post: (app, db) => {
         app.post('/image/upload', upload.single('file'), async (req, res) => {
-            const badgeImageData = ImageService.configureBadgeData(req);
-            const response = await ImageDataLayer.put(db, badgeImageData);
-            if (response.err) {
-                res.send({ "errorMessage": "Error uploading image", "errorMessageLong": "Error uploading image to database" })
-            } else {
-                res.send(response);
+            try {
+                const badgeImageData = ImageService.configureBadgeData(req);
+                const response = await ImageDataLayer.put(db, badgeImageData);
+                if (response.err) {
+                    res.send({ "errorMessage": "Error uploading image", "errorMessageLong": "Error uploading image to database" })
+                } else {
+                    res.send(response);
+                }
+            } catch {
+                res.send({ "errorMessage": "Error uploading image", "errorMessageLong": "Error uploading image" })
             }
+
         });
     },
     get: (app, db) => {
         app.get('/image/:id', async (req, res) => {
-            const imageID = ImageService.generateIdObjectForMongoSearch(req.params.id);
-            const response = await ImageDataLayer.get(db, imageID);
-            res.send(response);
+            try {
+                const imageID = ImageService.generateIdObjectForMongoSearch(req.params.id);
+                const response = await ImageDataLayer.get(db, imageID);
+                res.send(response);
+            } catch{
+                res.send({ "errorMessage": "Error retrieving image", "errorMessageLong": "Error retrieving image" })
+            }
         });
     }
 }
